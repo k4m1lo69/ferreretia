f2ed9a0124d4b2cb05ca299d97682de8e45dcd3a
Camilo Contreras <camil.contreras@duocuc.cl>
2026-05-31 23:38:27 -0400

    Creacion de microservicio Inventarios

---
+++ ms-inventarios/ms-inventarios/src/main/java/com/Ferreteria/ms_inventarios/MsInventariosApplication.java
@@ -0,0 +1,13 @@
+package com.Ferreteria.ms_inventarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsInventariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsInventariosApplication.class, args);
+	}
+
+}
---
+++ ms-inventarios/ms-inventarios/src/test/java/com/Ferreteria/ms_inventarios/MsInventariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ferreteria.ms_inventarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsInventariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
