03581d1f588dd21595b8ca57919c658a758d4916
Camilo Contreras <camil.contreras@duocuc.cl>
2026-06-01 08:33:52 -0400

    Creacion de repositorio UsuarioRepository

---
+++ ms-auth/ms-auth/src/main/java/com/Ferreteria/ms_auth/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.Ferreteria.ms_auth.repository;
+
+import com.Ferreteria.ms_auth.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+}
