61f67ea24cb63456c864a1ed80ad926acfc837ad
Camilo Contreras <camil.contreras@duocuc.cl>
2026-06-01 00:21:05 -0400

    Creacion de entidad Usuario en ms-auth

--- ms-auth/ms-auth/src/main/java/com/Ferreteria/ms_auth/model/Usuario.java
+++ ms-auth/ms-auth/src/main/java/com/Ferreteria/ms_auth/model/Usuario.java
@@ -1,4 +1,30 @@
 package com.Ferreteria.ms_auth.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String nombre;
+
+    @Column(nullable = false, length = 10)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String rol;
+
 }
