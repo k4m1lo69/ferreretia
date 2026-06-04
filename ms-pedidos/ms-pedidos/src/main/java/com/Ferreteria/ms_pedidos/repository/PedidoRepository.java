b247efac2efa20dbb2bf09b10afe9980d0ad9d97
Camilo Contreras <camil.contreras@duocuc.cl>
2026-06-04 16:25:35 -0400

    implementacion de repository y PedidoRepository

---
+++ ms-pedidos/ms-pedidos/src/main/java/com/Ferreteria/ms_pedidos/repository/PedidoRepository.java
@@ -0,0 +1,17 @@
+package com.Ferreteria.ms_pedidos.repository;
+
+
+import com.Ferreteria.ms_pedidos.model.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PedidoRepository
+        extends JpaRepository<Pedido, Long> {
+
+    List<Pedido> findByClienteId(Long clienteId);
+
+    List<Pedido> findByEstado(String estado);
+}
